763cd54bbbb8d530a8bdde09bc3b88823b391a69
moonjh99 <moonjh@datau.co.kr>
2026-01-04 15:41:58 +0900

    fix: Sentry 초기화 옵션 추가

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/react";
 const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
 
-console.log("VITE_SENTRY_DSN =", import.meta.env.VITE_SENTRY_DSN);
+console.log("VITE_SENTRY_DSN =", SENTRY_DSN);
 
 Sentry.init({
   // 프로젝트 식별
@@ -14,6 +14,21 @@ Sentry.init({
 
   // 전체 화면 이동 기준 성능만 수집
   tracesSampleRate: 0,
+
+  integrations: [
+    // 스택 트레이스 + 성능(선택)
+    Sentry.browserTracingIntegration(),
+
+    //n세션 리플레이
+    Sentry.replayIntegration({
+      maskAllText: false, // 보고용: 텍스트 보이게
+      blockAllMedia: false,
+    }),
+  ],
+
+  // Replay
+  replaysSessionSampleRate: 1.0, // 전체 세션 녹화
+  replaysOnErrorSampleRate: 1.0, // 에러 발생 시 100%
 
   // 수집 조건 제어
   beforeSend(event) {
